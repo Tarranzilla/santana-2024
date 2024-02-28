d0af2db58f80dc04a8989ec7b9e466d40f73ef85
JOAO TARRAN <joaotarran@gmail.com>
2024-02-28 15:43:58 -0300

    update

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -35,7 +35,7 @@ export default function Intro() {
                 {isMounted && (
                     <m.div initial={{ opacity: 1 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} className="Intro">
                         <div className="Intro_Content">
-                            <Image className="Intro_Image" src={"/brand_imgs/Logo_Tipo_SL_002.png"} alt="Logo" priority width={400} height={400} />
+                            <Image className="Intro_Image" src={"/brand_imgs/Logo_Tipo_SL_003.png"} alt="Logo" priority width={400} height={400} />
                         </div>
                     </m.div>
                 )}
